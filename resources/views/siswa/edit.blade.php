7d8527fc82d75428df8bef60df7af16651fe9ff1
slametannanas <kemalslamet29@gmail.com>
2024-05-23 18:24:14 +0700

    crud_done

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -36,33 +36,33 @@
               @endif
               <div class="col-md-12">
                 <label for="NIS" class="form-label">NIS</label>
-                <input type="text" class="form-control" id="NIS" name="NIS" value="{{ $siswa->NIS }}">
+                <input type="text" class="form-control" id="NIS" placeholder="Masukkan NIS Siswa" name="NIS" value="{{ $siswa->NIS }}">
               </div>
               
               <div class="col-md-6">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" value="{{ $siswa->email }}">
+                <input type="email" class="form-control" id="email" placeholder="Masukkan Email Siswa" name="email" value="{{ $siswa->email }}">
               </div>
               <div class="col-md-6">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password" value="{{ $siswa->password }}">
+                <input type="password" class="form-control" id="password" placeholder="Masukkan Password Siswa" name="password" value="{{ $siswa->password }}">
               </div>
               <div class="col-12">
                 <label for="nama_siswa" class="form-label">Nama Lengkap</label>
-                <input type="text" class="form-control" id="nama_siswa" placeholder="1234 Main St" name="nama_siswa" value="{{ $siswa->nama_siswa }}">
+                <input type="text" class="form-control" id="nama_siswa" placeholder="Masukkan Nama Lengkap Siswa" name="nama_siswa" value="{{ $siswa->nama_siswa }}">
               </div>
               <div class="col-12">
                 <label for="kelas" class="form-label">Kelas</label>
-                <input type="text" class="form-control" id="kelas" placeholder="Apartment, studio, or floor" name="kelas" value="{{ $siswa->kelas }}">
+                <input type="text" class="form-control" id="kelas" placeholder="Masukkan Kelas Siswa" name="kelas" value="{{ $siswa->kelas }}">
               </div>
               <div class="col-md-12">
                 <label for="ekstrakurikuler" class="form-label">Ekstrakurikuler</label>
-                <input type="text" class="form-control" id="ekstrakurikuler" name="ekstrakurikuler" value="{{ $siswa->ekstrakurikuler }}">
+                <input type="text" class="form-control" id="ekstrakurikuler" placeholder="Masukkan ekstrakurikuler yang diikuti Siswa" name="ekstrakurikuler" value="{{ $siswa->ekstrakurikuler }}">
               </div>
               <div class="col-md-12">
                 <label for="foto" class="col-sm-2 col-form-label">File Upload</label>
                 <div class="col-sm-10">
-                  <input class="form-control" type="file" id="foto" name="foto">
+                  <input class="form-control" type="file" id="foto" placeholder="Masukkan Foto Siswa" name="foto">
                 </div>
               </div>
               <div class="text-center">
